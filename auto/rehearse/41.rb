3d6b8df594d1d3e1233fe9f0eeaab7bcff521a79
joonseokii <joonseokii@yahoo.com>
2015-09-21 11:23:44 -0700

    Update 41.rb

--- auto/rehearse/41.rb
+++ auto/rehearse/41.rb
@@ -7,9 +7,19 @@ class Protocol
     o.input.all.take
     o.output.all.produce
 
-    show do
-      title "Instructions here"
-    end
+    show {
+      title "Autoclave: stick autoclave tape on top of bottle cap, loosen top and autoclave at 110C for 15 minutes."
+      timer initial: { hours: 0, minutes: 15, seconds: 0}
+    }
+    
+    show {
+      title "Remove from Autoclave: put on thermal gloves and take bottle out of autoclave, place on stir plate."
+      warning "Stuff caked at the bottom: after autoclaving, there is stuff caked at the bottom, do not use this batch, shake harder in step 5, make sure that everything is solvated before autoclaving"
+    }
+    
+    show {
+      title "Stir: Heat to 65C while stirring at 700 rpm."
+    }
 
     o.input.all.release
     o.output.all.release
