4bf4a32d5c03884a244ead008cba4b593097473a
TheNumbhead <garrett.s.newman@gmail.com>
2015-09-24 11:51:40 -0700

    Update 64.rb

--- auto/rehearse/64.rb
+++ auto/rehearse/64.rb
@@ -6,7 +6,13 @@ class Protocol
 
     o.input.all.take
     divided_yeast_plates = o.output.streaked_yeast_plate.new_collections
-
+    
+    o.threads.spread(divided_yeast_plates) do |t, slot| 
+      t.output.streaked_yeast_plate.associate slot
+    end
+    
+    o.output.streaked_yeast_plate.produce
+    
     show do
       title "Instructions here"
     end
