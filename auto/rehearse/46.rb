82696b3e9f800550013fc6182b59f7fa2e666d6b
joonseokii <joonseokii@yahoo.com>
2015-09-30 09:36:22 -0700

    Update 46.rb

--- auto/rehearse/46.rb
+++ auto/rehearse/46.rb
@@ -1,7 +1,7 @@
-
 class Protocol
 
   def main
+
     o = op input
 
     o.input.all.take
@@ -14,27 +14,23 @@ class Protocol
     }
     
     show {
-      title "Get Bottle and Stir Bar: Retrieve one Glass Liter Bottle from the glassware rack and one Medium Magnetic Stir Bar from the dishwashing station, bring to weigh station. Put the stir bar in the bottle."
+      note "Autoclave: stick autoclave tape on top of bottle cap, loosen top and autoclave at 110C for 15 minutes."
+      timer initial: { hours: 0, minutes: 15, seconds: 0}
     }
     
     show {
-      title "Weigh Agar: Using the gram scale, large weigh boat, and chemical spatula, weigh out 29.6 grams of LB Agar powder and pour into the bottle."
-      }
-      
-    show {
-      title "Using the graduated cylinder measure out 800 mL of DI water."
+      note "Remove from Autoclave: put on thermal gloves and take bottle out of autoclave, place on stir plate."
     }
     
     show {
-      title "Measure out Water: fill bottle with DI water, shake to get all powder solvated."
+      note "Stir: Heat to 65C while stirring at 700 rpm."
     }
-    
+
     o.input.all.release
     o.output.all.release
 
     return o.result
 
-  
   end
-  
+
 end
