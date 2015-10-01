da22f5eed341ca358a36ac9c1267032b5ee90e24
Eric Klavins <klavins@uw.edu>
2015-09-30 17:14:39 -0700

    Update 70.rb

--- auto/rehearse/70.rb
+++ auto/rehearse/70.rb
@@ -5,10 +5,22 @@ class Protocol
     o = op input
 
     o.input.all.take
-    o.output.all.produce
 
-    show do
-      title "Instructions here"
+    gels = o.input.gel.collections
+
+    # load the gel with ladder
+    gels.each do |gel|
+      gel.set 0, 0, o.input.ladder.sample_ids.first
+      gel.set 1, 0, o.input.ladder.sample_ids.first
+    end
+
+    # load the fragments
+    o.threads.spread(gels,skip_occupied: true) do |thread, slot| 
+      thread.output.fragment.associate slot
+    end
+
+    show do 
+      title "Run the gel"
     end
 
     o.input.all.release
