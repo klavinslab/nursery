ebb4e660c197683a38579898d3e6cee388fcb02c
Eric Klavins <klavins@uw.edu>
2015-09-30 17:14:16 -0700

    Update 69.rb

--- auto/rehearse/69.rb
+++ auto/rehearse/69.rb
@@ -1,21 +1,25 @@
-class Protocol
 
+class Protocol
+  
   def main
 
     o = op input
 
     o.input.all.take
+
+    show {
+      title "#{o.name} Inputs"
+      note "Detailed instructions for pouring a gel go here"
+    }
+
     o.output.all.produce
-
-    show do
-      title "Instructions here"
-    end
-
+    
     o.input.all.release
     o.output.all.release
 
-    return o.result
+    return o.result     
 
   end
 
 end
+
