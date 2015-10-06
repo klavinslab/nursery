be52d35ef988fb1555af4bb57d8ea9ea0d1b53dc
joonseokii <joonseokii@yahoo.com>
2015-10-05 22:03:09 -0700

    Update 41.rb

--- auto/rehearse/41.rb
+++ auto/rehearse/41.rb
@@ -6,21 +6,23 @@ class Protocol
 
     o.input.all.take
     o.output.all.produce
-    d = o.input.all
-    
+
     show {
-      title "Autoclave: stick autoclave tape on top of bottle cap, loosen top and autoclave at 110C for 15 minutes."
-      timer initial: { hours: 0, minutes: 15, seconds: 0}
-      note "#{d}"
+      title "Autoclave for Bacteria"
+      note "Description: This protocol is for sterilizing the media used for bacteria"
     }
     
     show {
-      title "Remove from Autoclave: put on thermal gloves and take bottle out of autoclave, place on stir plate."
-      warning "Stuff caked at the bottom: after autoclaving, there is stuff caked at the bottom, do not use this batch, shake harder in step 5, make sure that everything is solvated before autoclaving"
+      title "Tape Bottle"
+      note "Stick autoclave tape on top of the bottle"
+      warning "Make sure that the tape seals the cap to the bottle so that when you open the bottle you have to break the tape"
     }
     
     show {
-      title "Stir: Heat to 65C while stirring at 700 rpm."
+      title "Autoclave"
+      note "Check the water levels in the autoclave"
+      note "Loosen cap and autoclave at 110C for 15 minutes"
+      note "5 beeps will signify that the autoclave is done"
     }
 
     o.input.all.release
