887390be41d7df1a78c8080cfe4a39751efeeb82
TheNumbhead <garrett.s.newman@gmail.com>
2015-10-12 10:40:11 -0700

    Update 64.rb

--- auto/rehearse/64.rb
+++ auto/rehearse/64.rb
@@ -19,7 +19,7 @@ class Protocol
       title "Grab yeast plates"
         if o.output.streaked_yeast_plate.length > 0
           check "Grab #{o.output.streaked_yeast_plate.length} of YPAD plates, label with follow ids:"
-          note "#{o.output.streaked_yeast_plate}"
+          note "#{o.output.streaked_yeast_plate.length}"
           #note o.output.streaked_yeast_plate.collection.collect { |p| "#{p}"}
       #    check "Divide up each plate with 4 sections and mark each with circled #{(1..num_of_section).to_a.join(',')}"
           #image "divided_yeast_plate"
