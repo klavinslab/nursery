fce6c2073700c066ab33e6557b075dc84d063a44
TheNumbhead <garrett.s.newman@gmail.com>
2015-10-12 10:28:43 -0700

    Update 64.rb

--- auto/rehearse/64.rb
+++ auto/rehearse/64.rb
@@ -17,12 +17,12 @@ class Protocol
     
     show do
       title "Grab yeast plates"
-     #   if o.output.streaked_yeast_plate.length > 0
+        if o.output.streaked_yeast_plate.length > 0
       #    check "Grab #{o.output.streaked_yeast_plate.length} of YPAD plates, label with follow ids:"
       #    note o.output.streaked_yeast_plate.collect { |p| "#{p}"}
       #    check "Divide up each plate with 4 sections and mark each with circled #{(1..num_of_section).to_a.join(',')}"
       #    image "divided_yeast_plate"
-      #  end
+        end
     end
 
     # Take the required inputs from the lab
