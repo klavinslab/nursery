824d47823ed74c111f2f70a2a8125550aedc49c0
Aditya Gandhi <agandhi@uw.edu>
2015-10-31 00:03:15 -0700

    Some changes to this protocol

--- auto/rehearse/89.rb
+++ auto/rehearse/89.rb
@@ -5,12 +5,18 @@ class Protocol
     o = op input
 
     o.input.all.take
-    o.output.all.produce
-
-    show do
-      title "Instructions here"
+     show {
+      title "Protocol information"
+      note "This protocol is used to prepare yeast overnight suspensions from Divided Yeast Plate into Eppendorf 96 Deepwell Plate."
+      }
+    
+    deepwell_plates =o.output.yeast_deepwell_plate.new_collections
+    o.threads.spread(deepwell_plates,skip_occupied: true) do |t, slot|
+      t.output.yeast_deepwell_plate.associate slot
     end
-
+    
+    o.output.all.produce
+    
     o.input.all.release
     o.output.all.release
 
