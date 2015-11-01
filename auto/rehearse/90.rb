c86bffa8c943b1e71e988ff6b35543d436e8ddbc
Aditya Gandhi <agandhi@uw.edu>
2015-10-31 19:41:12 -0700

    Update 90.rb
    Some more changes. Not yet complete

--- auto/rehearse/90.rb
+++ auto/rehearse/90.rb
@@ -6,10 +6,14 @@ class Protocol
 
   o.input.all.take
   o.output.all.produce
+  #Currently these don't come as input
+  volume=
+  dilution_rate=
 
   input_yeast_deepwell_plates=o.input.yeast_deepwell_plate.collections
   output_yeast_deepwell_plates=o.output.yeast_deepwell_plate.new_collections
   
+  #Associating slots from the input and output deepwell plates
   o.threads.spread(output_yeast_deepwell_plates,skip_occupied: true) do |t, slot|
       t.input_yeast_deepwell_plates.associate slot
     end
@@ -18,14 +22,15 @@ class Protocol
       title "Take new deepwell plates"
       note "Grab #{output_yeast_deepwell_plates.length} Eppendorf 96 Deepwell Plate. Label with #{output_yeast_deepwell_plates.join(", ")}."
       output_yeast_deepwell_plates.each_with_index do |y,idx|
-        note "Add #{io_hash[:volume]*(1-io_hash[:dilution_rate])} µL of #{io_hash[:media_type]} into first #{num_of_wells[idx]} wells."
+        note "Add #{volume*(1-dilution_rate)} µL of 800 mL SC liquid(sterile) into first #{num_of_wells} wells."
       end
     }
     
     show {
       title "Vortex the deepwell plates."
-      note "Gently vortex the deepwell plates #{output_yeast_deepwell_plates.join(", ")} on a table top vortexer at settings 6 for about 20 seconds."
+      note "Gently vortex the deepwell plates #{input_yeast_deepwell_plates.join(", ")} on a table top vortexer at settings 6 for about 20 seconds."
     }
+  #Code to load the new Deepwell plates with inducers
 
   show {
       title "Seal the deepwell plate(s) with a breathable sealing film"
@@ -35,7 +40,7 @@ class Protocol
 
     show {
       title "Place the deepwell plates in the washing station"
-      note "Place the following deepwell plates #{output_yeast_deepwell_plates.join(", ")} in the washing station "
+      note "Place the following deepwell plates #{input_yeast_deepwell_plates.join(", ")} in the washing station "
     }
 
     o.input.all.release
