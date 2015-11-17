9e9ebfc0f7550a2d652530949096158af5a7be42
Yaoyu Yang <yaoyu.yang88@gmail.com>
2015-11-16 18:10:31 -0800

    Update 106.rb

--- auto/rehearse/106.rb
+++ auto/rehearse/106.rb
@@ -5,6 +5,16 @@ class Protocol
     o = op input
 
     o.input.all.take
+    
+    # Create all the collections needed to store the the samples called 'part'
+    # summed over all threads.
+    yeast_deepwell_plates = o.output.yeast_deepwell_plate.new_collections
+
+    # Associate the samples called 'part' with the slots in the collections.
+    o.threads.spread(yeast_deepwell_plates) do |t, slot|
+      t.output.yeast_deepwell_plate.associate slot
+    end
+    
     o.output.all.produce
 
     show do
