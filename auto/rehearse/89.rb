f70dacb77bf3d1d5b2108d284959550e27e5d757
Yaoyu Yang <yaoyu.yang88@gmail.com>
2015-11-16 16:29:27 -0800

    update

--- auto/rehearse/89.rb
+++ auto/rehearse/89.rb
@@ -16,7 +16,7 @@ class Protocol
     op_deepwell_plates = o.output.yeast_deepwell_plate.new_collections
 
     show {
-      note "#{o.output.yeast_deepwell_plate.to_json}"
+      note "#{o.output.yeast_deepwell_plate.item_ids}"
       note op_deepwell_plates.each { |deepwell| "#{deepwell.id}"}
     }
 
