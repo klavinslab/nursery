f4680f1367f609600466439555e9f9f972d668df
Yaoyu Yang <yaoyu.yang88@gmail.com>
2015-11-16 16:41:48 -0800

    update

--- auto/rehearse/89.rb
+++ auto/rehearse/89.rb
@@ -1,9 +1,56 @@
 class Protocol
 
+  module Reporter
+    def report part
+      warning "error: #{part.errors.join(', ')}" if part.errors
+      yield part unless part.errors
+    end
+  end
+
+  def show_details io, str
+
+    show do
+
+      extend Reporter
+
+      title str
+
+      report(io.samp) do |samp|
+        note "samp: #{samp.sample} => Item #{samp.item}"
+      end
+
+      report(io.samp_vec) do |samp_vec|
+        note "samp_vec: #{samp_vec.samples.join(', ')} => Items #{samp_vec.items.join(', ')}"
+      end
+
+      report(io.part) do |part|
+        note "part #{part.sample} => Collection #{part.collection}, row #{part.row}, colum #{part.column}"
+      end
+
+      report(io.part_vec) do |part_vec|
+        collections = part_vec.collections
+        (0..collections.length-1).each do |i|
+          note "part_vec(#{i}) => Collection #{collections[i]}, row #{part_vec.rows[i]}, column #{part_vec.columns[i]}"
+        end
+      end
+
+      report (io.simple) do |simple|
+        note "simple #{simple.container} => Item #{simple.item}"
+      end
+
+    end
+
+  end
+
   def main
     o = op input
 
     o.input.all.take
+
+    # Loop through each thread and show the results of the take.
+    o.threads.each do |thread|
+      show_details thread.input, "Thread #{thread.index} Inputs"
+    end
 
     show {
       title "Protocol information"
@@ -22,6 +69,12 @@ class Protocol
     # Produce all the outputs. This associated the outputs with the job so
     # the can be listed in the log.
     o.output.all.produce
+
+    # Loop through each thread and show the results of the associations and produces
+    o.threads.each do |thread|
+      show_details thread.output, "Thread #{thread.index} Outputs"
+    end
+
 
     show {
       note "#{o.output.yeast_deepwell_plate.item_ids}"
