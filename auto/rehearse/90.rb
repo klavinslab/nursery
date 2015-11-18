f6cefa221b8e8ca087f6ff5302cad79cdb26d099
Aditya Gandhi <agandhi@uw.edu>
2015-11-18 10:53:33 -0800

    Update 90.rb

--- auto/rehearse/90.rb
+++ auto/rehearse/90.rb
@@ -12,12 +12,12 @@ class Protocol
   #dilution_rate=
 
   ip_deepwell_plates=o.input.yeast_deepwell_plate.collections
-  o_deepwell_plates=o.output.yeast_deepwell_plate.new_collections
+  op_deepwell_plates=o.output.yeast_deepwell_plate.new_collections
 
  show {
       title "Take new deepwell plates"
-      note "Grab #{o_yeast_deepwell_plates.length} Eppendorf 96 Deepwell Plate. Label with #{o_yeast_deepwell_plates.join(", ")}."
-      output_yeast_deepwell_plates.each_with_index do |y,idx|
+      note "Grab #{op_deepwell_plates.length} Eppendorf 96 Deepwell Plate. Label with #{op_deepwell_plates.join(", ")}."
+      op_deepwell_plates.each_with_index do |y,idx|
         note "Add #{volume*(1-dilution_rate)} µL of 800 mL SC liquid(sterile) into first #{num_of_wells} wells."
       end
     }
