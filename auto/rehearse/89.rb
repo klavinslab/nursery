17493ab0dd63bd7bf91a595df2e0662f431396db
Aditya Gandhi <agandhi@uw.edu>
2015-11-18 10:24:41 -0800

    Update 89.rb

--- auto/rehearse/89.rb
+++ auto/rehearse/89.rb
@@ -12,9 +12,9 @@ class Protocol
 
     #Create new collections, in this case Eppendorf 96 Deepwell Plate/Plates
     #This is thread aware and takes into account the yeast strains specified in each of threads batched for execution
-    op_deepwell_plates = o.output.yeast_strain.new_collections
+    deepwell_plates = o.output.yeast_strain.new_collections
 
-    o.threads.spread(op_deepwell_plates, skip_occupied: true) do |t, slot|
+    o.threads.spread(deepwell_plates, skip_occupied: true) do |t, slot|
       #Associating a yeast strain with available slots in the newly produced collection
       t.output.yeast_strain.associate slot
     end
@@ -22,7 +22,7 @@ class Protocol
     # Produce all the outputs. This associated the outputs with the job so
     # the can be listed in the log.
     o.output.all.produce
-    #
+    
     #  #Creating the table to show yeast strain-> Collection->Inducer association to the technicians
     t = Table.new output_collection_id:"Eppendorf 96 Deepwell Plate",output_collection_loc:"Location",liquid:"800ml SC liquid (sterile)",ip_div_plate:"Divided Yeast Plate",ip_div_location:"Location",inducer:"Inducer"
     o.threads.each do |thread|
@@ -33,13 +33,24 @@ class Protocol
     end
     #
     #Rendering the table
-     show do
+    show do
        table t.choose([:output_collection_id,:output_collection_loc,:liquid,:ip_div_plate,:ip_div_location,:inducer]).render
      end
 
+    show {
+      title "Seal the deepwell plate(s) with a breathable sealing film"
+      note "Put a breathable sealing film on following deepwell plate(s) #{deepwell_plates.join(", ")}."
+      note "Place the deepwell plate(s) into the 30 C shaker incubator, make sure it is secure."
+    }
+    
+    deepwell_plates.each do |d|
+      d.location = "30 C shaker incubator"
+      d.save
+    end
+    
+    #Release the input
     o.input.all.release
-    o.output.all.release
-
+    
     return o.result
   end
 
