571bf268a1d07b0beff1211a54c0f796ffba4a97
joonseokii <joonseokii@yahoo.com>
2015-11-24 02:02:58 -0800

    Update 30.rb

--- auto/production/30.rb
+++ auto/production/30.rb
@@ -8,8 +8,13 @@ class Protocol
     o.output.all.produce
 
     show do
-      title "Instructions here"
       note "#{o.output.media.sample_names}"
+      sampleString = "SDO -trp -leu"
+      contentChecker = Array.new
+      inputAcids = sampleString.split('')
+      (0..array.length-1).each do |i|
+        contentChecker.push(acids)
+      
     end
 
     o.input.all.release
