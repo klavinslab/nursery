d48086f50ac1911d21448b36860acd14a80b25c0
joonseokii <joonseokii@yahoo.com>
2015-11-25 11:08:18 -0800

    Update 30.rb

--- auto/production/30.rb
+++ auto/production/30.rb
@@ -15,16 +15,16 @@ class Protocol
     typeSDO = label.scan(/-[a-z]+/)
     acids = typeSDO.collect{|x| x.gsub(/-/, '')}
     includeAcids = ["leu", "his", "trp", "ura"] - acids
-    includeAcids.each do |i|
-      if(i == "leu")
-        ingredients += find(:item,{object_type:{name:"Leucine Solution"}})
-      elsif(i == "his")
-        ingredients += find(:item,{object_type:{name:"Histidine Solution"}})
-      elsif(i == "trp")
-        ingredients += find(:item,{object_type:{name:"Tryptophan Solution"}})
-      else
-        ingredients += find(:item,{object_type:{name:"Uracil Solution"}})
-    end
+    #includeAcids.each do |i|
+    #  if(i == "leu")
+    #    ingredients += find(:item,{object_type:{name:"Leucine Solution"}})
+    #  elsif(i == "his")
+    #    ingredients += find(:item,{object_type:{name:"Histidine Solution"}})
+    #  elsif(i == "trp")
+    #    ingredients += find(:item,{object_type:{name:"Tryptophan Solution"}})
+    #  else
+    #    ingredients += find(:item,{object_type:{name:"Uracil Solution"}})
+    #end
     
     take ingredients, interactive: true
     item_id = o.output.all.item_ids
