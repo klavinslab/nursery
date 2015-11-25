97716aacbbb6f4f80115c6cb4193a5514639cfde
joonseokii <joonseokii@yahoo.com>
2015-11-25 10:54:00 -0800

    Update 30.rb

--- auto/production/30.rb
+++ auto/production/30.rb
@@ -40,7 +40,7 @@ class Protocol
     
     show {
       title "Label Bottle"
-      note "Label the bottle with 'YPAD', 'Your initials', '#{item_id[0]}'"
+      note "Label the bottle with 'YPAD', 'Date', Your initials', '#{item_id[0]}'"
     }
     
     release ingredients, interactive: true
