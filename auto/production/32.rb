c78572aa2b5d032a70be2742853f1973f0b653c2
joonseokii <joonseokii@yahoo.com>
2015-11-25 10:59:40 -0800

    Update 32.rb

--- auto/production/32.rb
+++ auto/production/32.rb
@@ -6,10 +6,35 @@ class Protocol
 
     o.input.all.take
     o.output.all.produce
-
-    show do
-      title "Instructions here"
+    
+    type = o.input.all.samples
+    label = type.gsub(/(unsterile)/,'')
+    
+    if (type.include? "LB" || type.include? "TB")
+      temp = 121
+    elsif(type.include? "SDO" || type.include? "SC" || type.include? "YPAD")
+      temp = 110
+    else 
+      raise ArgumentError, 'Parameter is not valid'
     end
+      
+    show {
+      title "Autoclave Media"
+      note "Description: This protocol is for sterilizing the media used for #{label}"
+    }
+    
+    show {
+      title "Tape Bottle"
+      note "Stick autoclave tape on top of the bottle"
+      warning "Make sure that the tape seals the cap to the bottle so that when you open the bottle you have to break the tape"
+    }
+    
+    show {
+      title "Autoclave"
+      note "Check the water levels in the autoclave"
+      note "Loosen cap and autoclave at #{temp}C for 15 minutes"
+      note "5 beeps will signify that the autoclave is done"
+    }
 
     o.input.all.release
     o.output.all.release
