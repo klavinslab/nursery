b1673c3355859a44963b73428fd86d6431d43de3
joonseokii <joonseokii@yahoo.com>
2015-11-25 11:19:58 -0800

    Update 33.rb

--- auto/production/33.rb
+++ auto/production/33.rb
@@ -18,13 +18,13 @@ class Protocol
     includeAcids = ["leu", "his", "trp", "ura"] - acids
     includeAcids.each do |i|
       if(i == "leu")
-        ingredients += find(:item,{object_type:{name:"Leucine Solution"}})[0]
+        ingredients += find(:item,{object_type:{name:"Leucine Solution"}})
       elsif(i == "his")
-        ingredients += find(:item,{object_type:{name:"Histidine Solution"}})[0]
+        ingredients += find(:item,{object_type:{name:"Histidine Solution"}})
       elsif(i == "trp")
-        ingredients += find(:item,{object_type:{name:"Tryptophan Solution"}})[0]
+        ingredients += find(:item,{object_type:{name:"Tryptophan Solution"}})
       else
-        ingredients += find(:item,{object_type:{name:"Uracil Solution"}})[0]
+        ingredients += find(:item,{object_type:{name:"Uracil Solution"}})
       end
     end
     
