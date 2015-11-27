a6b3f3fd807268d7fd8fe0b888ecd7ea8824d240
Eric Klavins <klavins@uw.edu>
2015-11-27 14:28:03 -0800

    Update 34.rb

--- auto/production/34.rb
+++ auto/production/34.rb
@@ -5,10 +5,20 @@ class Protocol
     o = op input
 
     o.input.all.take
-    o.output.all.produce
+
+    pLAB1 = Sample.find_by_name("pLAB1")
+    raise ( "Could not find pLAB1" ) unless pLAB1
+
+    o.output.y.associate_sample(pLAB1).produce
 
     show do
-      title "Instructions here"
+      title "Threads"
+      o.threads.each do |thread|
+        note "Thread #{thread.index}"
+        bullet "u1: #{thread.input.u1.sample.name} (#{thread.input.u1.sample}), Item: #{thread.input.u1.item}"
+        bullet "u2: #{thread.input.u2.sample.name} (#{thread.input.u2.sample}), Item: #{thread.input.u2.item}"
+        bullet "y: #{thread.output.y.sample.name} (#{thread.output.y.sample}), Item: #{thread.output.y.item}"
+      end
     end
 
     o.input.all.release
