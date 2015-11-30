12669a952d837a6b496a8995b2189e78355f17bd
joonseokii <joonseokii@yahoo.com>
2015-11-30 11:54:09 -0800

    Update 33.rb

--- auto/production/33.rb
+++ auto/production/33.rb
@@ -1,40 +1,69 @@
 class Protocol
   
   def main
+    
     o = op input
     
-    o.output.all.produce
-    label = o.output.all.get[0].name
-    
-    ingredients = find(:item,{object_type:{name:"Adenine (Adenine hemisulfate)"}}) + 
-        find(:item,{object_type:{name:"Dextrose"}}) + find(:item,{object_type:{name:"Yeast Nitrogen Base Without Amino Acids"}}) 
-            
-    if(label.include?("agar"))
-      ingredients += find(:item, {object_type:{name:"Bacto Tryptone"}})
-    end
-    
-    typeSDO = label.scan(/-[a-z]+/)
-    acids = typeSDO.collect{|x| x.gsub(/-/, '')}
-    includeAcids = ["leu", "his", "trp", "ura"] - acids
-    includeAcids.each do |i|
-      if(i == "leu")
-        ingredients += find(:item,{object_type:{name:"Leucine Solution"}})
-      elsif(i == "his")
-        ingredients += find(:item,{object_type:{name:"Histidine Solution"}})
-      elsif(i == "trp")
-        ingredients += find(:item,{object_type:{name:"Tryptophan Solution"}})
+    tempAcids = Array.new
+  
+    includeAcids = o.input.powder
+    includeAcids.each do |acid|
+      if(acid == "Histidine")
+        tempAcids.push("His")
+      elsif(acid == "Leucine")
+        tempAcids.push("Leu")
+      elsif(acid == "Tryptophan")
+        tempAcids.push("Trp")
+      elsif(acid == "Uracil")
+        ary.push("Ura")
       else
-        ingredients += find(:item,{object_type:{name:"Uracil Solution"}})
+        raise("Non amino acid specified")
       end
     end
     
-    take ingredients, interactive: true
+    missingAcids = ["His", "Leu", "Trp", "Ura"] - tempAcids
+    
+    if(tempAcids.empty?)
+      label = "SC (unsterile)"
+    else
+      label = "SDO -" + missingAcids.join(" -")
+    
+    dropOut = Sample.find_by_name("")
+    raise ( "Could not find Media" ) unless dropOut
+    
+    
+    o.input.all.take
+    o.output.all.produce
+    
+    #ingredients = find(:item,{object_type:{name:"Adenine (Adenine hemisulfate)"}}) + 
+    #    find(:item,{object_type:{name:"Dextrose"}}) + find(:item,{object_type:{name:"Yeast Nitrogen Base Without Amino Acids"}}) 
+    
+
+    # if(o.input.all.include?("agar"))
+    #  ingredients += find(:item, {object_type:{name:"Bacto Tryptone"}})
+    #end
+    
+    #typeSDO = label.scan(/-[a-z]+/)
+    #acids = typeSDO.collect{|x| x.gsub(/-/, '')}
+    #includeAcids = ["leu", "his", "trp", "ura"] - acids
+    #includeAcids.each do |i|
+    #  if(i == "leu")
+    #    ingredients += find(:item,{object_type:{name:"Leucine Solution"}})
+    #  elsif(i == "his")
+    #    ingredients += find(:item,{object_type:{name:"Histidine Solution"}})
+    #  elsif(i == "trp")
+    #    ingredients += find(:item,{object_type:{name:"Tryptophan Solution"}})
+    #  else
+    #    ingredients += find(:item,{object_type:{name:"Uracil Solution"}})
+    #  end
+    #end
+    
+    #take ingredients, interactive: true
 
     
     show {
       title "#{label}"
       note "Description: Makes 800 mL of #{label} media with 2% glucose and adenine supplement"
-      note "#{label}"
     }
     
     show {
@@ -44,7 +73,7 @@ class Protocol
     
     show {
       title "Weigh Chemicals"
-      note "Weight out 5.36g nitrogen base, 1.12g of DO media, 16g of dextrose, .064g adenine sulfate#{label.include?("agar") ? ", 16g tryptone" : ""} and add to 1000 mL bottle"
+      #note "Weight out 5.36g nitrogen base, 1.12g of DO media, 16g of dextrose, .064g adenine sulfate#{o.input.all.include?("agar") ? ", 16g tryptone" : ""} and add to 1000 mL bottle"
     }
     
     show {
