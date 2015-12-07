73c96f94b1dba3c33e28abb990c6e96d62e914ce
joonseokii <joonseokii@yahoo.com>
2015-12-07 02:39:50 -0800

    Update 33.rb

--- auto/production/33.rb
+++ auto/production/33.rb
@@ -69,7 +69,7 @@ class Protocol
     show {
       title "#{label}"
       note "Description: Makes 800 mL of #{label} media with 2% glucose and adenine supplement"
-      note "#{o.input.all}"
+      note "#{o.input.powder[0].sample.name}"
     }
     
     show {
