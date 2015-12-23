18f3c492422a8a5af66765018eeea326a73d95b3
joonseokii <joonseokii@yahoo.com>
2015-12-23 15:34:10 -0800

    Update 33.rb

--- auto/production/33.rb
+++ auto/production/33.rb
@@ -44,7 +44,7 @@ class Protocol
       dropOut = Sample.find_by_name(label)
       raise ( "Could not find Media" ) unless dropOut
       
-      o.output.media.associate_sample(dropOut).produce
+      thread.output.media.associate_sample(dropOut).produce
 
       show {
         title "#{label}"
